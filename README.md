2e802c3a27f89a5c7bc5c039cd75d067c4754a84
Methapon2001 <dark.methapon13@gmail.com>
2021-11-28 14:14:12 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Deno App
+
+Creating your own REST API with deno and oak.
+
+## Prerequisite
+- [Deno](https://deno.land/)
+- [Velociraptor](https://deno.land/x/velociraptor)
+
+## Run your deno app
+- Use `vr run dev` to start development server.
+- Use `vr run start` to start production server.
---
+++ index.ts
@@ -0,0 +1,21 @@
+import { Application, Router } from "oak";
+
+const app = new Application();
+const router = new Router();
+
+router.get("/", ({ response }) => {
+  response.status = 200;
+  response.body = {
+    result: 'success',
+    message: 'Hello World!',
+  };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.addEventListener("listen", ({ hostname, port }) => {
+  console.log(`Listening on ${hostname}:${port}`);
+});
+
+await app.listen({ port: parseInt(Deno.env.get('PORT') || '8080') });
